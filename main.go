0e3a10ff775388c3fe1b1f35f45adf78be9cbf24
llitfkitfk <llitfkitfk@gmail.com>
2017-03-31 14:58:45 +0800

    format

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("view/static/"))
+	fs := http.FileServer(http.Dir("web/static/"))
 	http.Handle("/static/", http.StripPrefix("/static/",fs))
 	http.HandleFunc("/", seveIndex)
 	log.Println("Listening...")
 	http.ListenAndServe(":8080", nil)
 }
 func seveIndex(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("view/index.html"))
+	tmpl := template.Must(template.ParseFiles("web/index.html"))
 	tmpl.Execute(w, nil)
 }
